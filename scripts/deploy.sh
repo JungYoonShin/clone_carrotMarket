ad2110c7aa0bb4735d9469fd70c6d0f9c6dd0690
JungYoonShin <sju08227@naver.com>
2023-05-15 15:43:10 +0900

    ci/cd 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-REPOSITORY=/home/ubuntu/app
+BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy.log
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo ">>> build 파일 복사" >> /home/ubuntu/app/deploy.log
+DEPLOY_PATH=/home/ubuntu/app/
+cp $BUILD_JAR $DEPLOY_PATH
 
-CURRENT_PID=$(pgrep -fla java | grep carrot | awk '{print $1}')
+echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]
+then
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar \
-        -Duser.timezone=Asia/Seoul \
-        -Dspring.config.location=/home/ubuntu/app/application.yml \
-        $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/app/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/app/deploy_err.log &
