5d02cb922781a2263f20a627daa9f97f143c4fdd
JungYoonShin <sju08227@naver.com>
2023-05-16 21:39:39 +0900

    테스트

--- src/main/java/com/example/carrotmarket/controller.java
+++ src/main/java/com/example/carrotmarket/controller.java
@@ -1,5 +1,8 @@
 package com.example.carrotmarket;
 
+import com.example.carrotmarket.entity.User;
+import com.example.carrotmarket.entity.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/")
     public String hi() {
-        return "hellllooo";
+        User user = new User();
+        userRepository.save(user);
+        return "djlksjdlf";
     }
 }
---
+++ src/main/java/com/example/carrotmarket/entity/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.carrotmarket.entity;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
