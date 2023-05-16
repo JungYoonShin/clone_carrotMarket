29c2ba2b514f654994ee2d5964b4c7411ceed9c1
JungYoonShin <sju08227@naver.com>
2023-05-16 21:51:30 +0900

    테스트

--- src/main/java/com/example/carrotmarket/controller.java
+++ src/main/java/com/example/carrotmarket/controller.java
@@ -2,6 +2,7 @@ package com.example.carrotmarket;
 
 import com.example.carrotmarket.entity.User;
 import com.example.carrotmarket.entity.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/login")
+@RequiredArgsConstructor
 public class controller {
-//    @Autowired
-//    UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @GetMapping("/dd")
+    @GetMapping("/hi")
     public String hi() {
 //        User user = new User();
 //        userRepository.save(user);
