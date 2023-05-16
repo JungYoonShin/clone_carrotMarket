3b8c3763e3f775c46e7d75eb39756a54073e0f9e
JungYoonShin <sju08227@naver.com>
2023-05-16 20:55:54 +0900

    테스트

---
+++ src/main/java/com/example/carrotmarket/entity/User.java
@@ -0,0 +1,19 @@
+package com.example.carrotmarket.entity;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "userss")
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+}
