6d58a34039c419efdc254e168486f524d0276cd6
JungYoonShin <sju08227@naver.com>
2023-05-18 15:48:23 +0900

    .

--- src/main/java/com/example/carrotmarket/entity/UserRepository.java
+++ src/main/java/com/example/carrotmarket/entity/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
